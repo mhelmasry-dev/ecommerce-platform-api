0d927bdcf790602fbe48031b0dd994fb49db9339
Mohammed-Hassan-Saad-Elmasry <mohammedhassanelmasry881@gmail.com>
2026-03-31 01:46:27 +0200

    feat: add full swagger documentation for all 12 modules/

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ import bootstrap from "./index.router.js";
 import swaggerUi from "swagger-ui-express";
 import swaggerSpec from "./swagger.js";
 const app = express();
-app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+app.use("/api-docs", swaggerUi.serve);
+app.get("/api-docs", swaggerUi.setup(swaggerSpec));
 const port = process.env.PORT || 5000;
 bootstrap(app, express);
 app.listen(port, () => {
